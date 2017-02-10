0c68d4320d2bfa1fd8b2c3e8e595d81e75c97b59
David Ray <dray@caktusgroup.com>
2017-02-10 08:56:53 -0500

    readding as this is used in settings/__init__.py for local dev and Vagrant

---
+++ bakerydemo/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# BASE_URL required for notification emails
+BASE_URL = 'http://localhost:8000'
+
+try:
+    from .local import *
+except ImportError:
+    pass
