20ac81f6ca48d655df0fa76a45577a816156f22c
David Ray <dray@caktusgroup.com>
2017-02-10 08:12:37 -0500

    Execute load_initial data when provisioning Vagrant box

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -32,8 +32,9 @@ su - vagrant -c "$PIP install -r $PROJECT_DIR/requirements.txt"
 chmod a+x $PROJECT_DIR/manage.py
 
 
-# Run syncdb/migrate/update_index
+# Run syncdb/migrate/load_initial_data/update_index
 su - vagrant -c "$PYTHON $PROJECT_DIR/manage.py migrate --noinput && \
+                 $PYTHON $PROJECT_DIR/manage.py load_initial_data && \
                  $PYTHON $PROJECT_DIR/manage.py update_index"
 
 
