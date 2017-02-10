9fa96f61c2c8530b7de9c29989d96176a665978d
David Ray <dray@caktusgroup.com>
2017-02-10 10:57:18 -0500

    tweak for heroku

--- bakerydemo/settings/heroku.py
+++ bakerydemo/settings/heroku.py
@@ -8,7 +8,7 @@ DEBUG = True
 # Accept all hostnames, since we don't know in advance which hostname will be used for any given Heroku instance.
 # IMPORTANT: Set this to a real hostname when using this in production!
 # See https://docs.djangoproject.com/en/1.10/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'wagtailbakerydemo.herokuapp.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
