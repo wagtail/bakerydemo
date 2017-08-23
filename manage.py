8ae35061107e07e2bf303a77bdd3172ce171a9e1
Mikalai Radchuk <radchuk.m@gmail.com>
2017-08-23 11:28:02 +0100

    Set DJANGO_SETTINGS_MODULE to match wsgi.py

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ import dotenv
 if __name__ == "__main__":
     dotenv.read_dotenv()
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bakerydemo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bakerydemo.settings.dev")
 
     from django.core.management import execute_from_command_line
 
