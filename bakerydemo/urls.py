0f35cb1bebcc49a78c90020a673dadd050e7ac48
Matt Westcott <matt@west.co.tt>
2018-05-22 21:14:14 +0100

    Enable sitemap

--- bakerydemo/urls.py
+++ bakerydemo/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 
 from bakerydemo.search import views as search_views
@@ -16,6 +17,7 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
 
+    url('^sitemap\.xml$', sitemap),
 ]
 
 
