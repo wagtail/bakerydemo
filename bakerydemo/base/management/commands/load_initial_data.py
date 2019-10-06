220074d19517a08b3a8f162ca3f0ec52135341aa
Tobias McNulty <tobias@caktusgroup.com>
2019-10-05 22:20:02 -0400

    copy files from MEDIA_ROOT to remote storage during load_initial_data

--- bakerydemo/base/management/commands/load_initial_data.py
+++ bakerydemo/base/management/commands/load_initial_data.py
@@ -1,6 +1,7 @@
 import os
 
 from django.conf import settings
+from django.core.files.storage import default_storage, FileSystemStorage
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
@@ -8,7 +9,25 @@ from wagtail.core.models import Site, Page
 
 
 class Command(BaseCommand):
+    def _copy_files(self, local_storage, path):
+        """
+        Recursively copy files from local_storage to default_storage. Used
+        to automatically bootstrap buckets on cloud providers with the
+        images linked from the initial data (and included in MEDIA_ROOT).
+        """
+        directories, file_names = local_storage.listdir(path)
+        for directory in directories:
+            self._copy_files(local_storage, path + directory + '/')
+        for file_name in file_names:
+            with local_storage.open(path + file_name) as file_:
+                default_storage.save(path + file_name, file_)
+
     def handle(self, **options):
+        if not isinstance(default_storage, FileSystemStorage):
+            print("It looks like you're using a non-local file storage. Copying media files to remote...")
+            local_storage = FileSystemStorage(settings.MEDIA_ROOT)
+            self._copy_files(local_storage, '')  # file storage paths are relative
+
         fixtures_dir = os.path.join(settings.PROJECT_DIR, 'base', 'fixtures')
         fixture_file = os.path.join(fixtures_dir, 'bakerydemo.json')
 
