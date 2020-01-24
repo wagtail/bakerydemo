34e6e874787e790943e55dce7461991c8ef811b4
jacobtm <jacobtm@torchbox.com>
2020-01-24 15:28:31 +0000

    Add media app with custom image model, and migrations

---
+++ media/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from wagtail.documents.models import AbstractDocument
+from wagtail.images.models import AbstractImage, AbstractRendition
+
+from taggit.managers import TaggableManager
+
+
+class Image(AbstractImage):
+    admin_form_fields = (
+        'title',
+        'file',
+        'collection',
+        'tags',
+        'focal_point_x',
+        'focal_point_y',
+        'focal_point_width',
+        'focal_point_height',
+    )
+
+    tags = TaggableManager(help_text=None, blank=True, verbose_name=_('tags'), related_name='images')
+
+    class Meta:
+        verbose_name = _('image')
+        verbose_name_plural = _('images')
+
+
+class Rendition(AbstractRendition):
+    image = models.ForeignKey(Image, related_name='renditions', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (
+            ('image', 'filter_spec', 'focal_point_key'),
+        )
+
+
+class Document(AbstractDocument):
+    admin_form_fields = (
+        'title',
+        'file',
+        'collection',
+        'tags'
+    )
+
+    tags = TaggableManager(help_text=None, blank=True, verbose_name=_('tags'), related_name='documents')
