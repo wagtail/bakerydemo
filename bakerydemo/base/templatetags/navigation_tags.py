3372510e60083a7f258a1f11233ebefe7415c3a3
Matt Westcott <matt@west.co.tt>
2020-05-05 12:27:58 +0100

    Remove use of request.site / SiteMiddleware

--- bakerydemo/base/templatetags/navigation_tags.py
+++ bakerydemo/base/templatetags/navigation_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Site
 
 from bakerydemo.base.models import FooterText
 
@@ -14,7 +14,7 @@ def get_site_root(context):
     # This returns a core.Page. The main menu needs to have the site.root_page
     # defined else will return an object attribute error ('str' object has no
     # attribute 'get_children')
-    return context['request'].site.root_page
+    return Site.find_for_request(context['request']).root_page
 
 
 def has_menu_children(page):
--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -76,7 +76,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 
 ]
