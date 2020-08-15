5d255cd535cd9269fbb286f7db4ab8a5835d8af1
Cynthia Kiser <cnk@caltech.edu>
2020-08-15 14:18:21 -0700

    upgrade to Wagtail 2.10

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'bakerydemo.locations',
     'bakerydemo.search',
 
+    'wagtail.contrib.legacy.richtext',
     'wagtail.contrib.search_promotions',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
