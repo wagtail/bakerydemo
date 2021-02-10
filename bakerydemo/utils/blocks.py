296cd64b8f0d0d7b73186f4f610c160b5b0a673b
Brian Xu <brian.xu@torchbox.com>
2021-02-10 14:48:21 +0800

    add detail to sequence block

--- bakerydemo/utils/blocks.py
+++ bakerydemo/utils/blocks.py
@@ -1,8 +1,53 @@
+from django.db import models
 from wagtail.core import blocks
+
+# Struct blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class ButtonBlock(blocks.StructBlock):
+    label = blocks.CharBlock(required=True)
+    link = blocks.URLBlock(required=True)
+
+    class Meta:
+        icon = "fa-play-circle"
+
+
+class LinkBlock(blocks.StructBlock):
+    label = blocks.CharBlock(required=True)
+    link = blocks.URLBlock(required=True)
+
+    class Meta:
+        icon = "fa-link"
+
+
+class AccordionBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True)
+    content = blocks.RichTextBlock(required=True)
+
+    class Meta:
+        icon = "fa-list"
+
+
+class CardBlock(blocks.StructBlock):
+    image = ImageChooserBlock()
+    title = blocks.CharBlock(required=True)
+    body = blocks.StreamBlock([
+        ('paragraph', blocks.RichTextBlock()),
+        ('button', ButtonBlock()),
+        ('accordion', blocks.ListBlock(AccordionBlock())),
+        ('link', LinkBlock())
+    ],required=True)
+
+    class Meta:
+        icon = "fa-square-o"
 
 
 class CardSequenceBlock(blocks.StructBlock):
-    pass
+    cards = blocks.ListBlock(CardBlock())
+
+    class Meta:
+        icon = "fa-list"
 
 
 class LongBlock(blocks.StructBlock):
