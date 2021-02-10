aa0789011a1e8a0e6a469e7a1d49a9189eacdc13
Brian Xu <brian.xu@torchbox.com>
2021-02-10 13:01:46 +0800

    create landing app

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'bakerydemo.breads',
     'bakerydemo.locations',
     'bakerydemo.search',
+    'bakerydemo.landing',
 
     'wagtail.contrib.search_promotions',
     'wagtail.contrib.forms',
