dec0f341817c9b15336084a3d455c4bf294695f6
Brian Xu <brian.xu@torchbox.com>
2021-03-08 17:08:05 +0800

    Format settings file

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -106,7 +105,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bakerydemo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -116,7 +114,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'bakerydemodb')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -136,7 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -149,7 +145,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
