71e6c67872f12601662a96265679bf956bbd4a06
Jake Howard <jake.howard@torchbox.com>
2022-11-18 15:10:25 +0000

    Use more feature-packed configuration for S3 storage

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -251,15 +251,62 @@ AWS_REGION = os.getenv("AWS_REGION", "")
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
+# AWS S3 buckets configuration
+# This is media files storage backend configuration. S3 is our preferred file
+# storage solution.
+# To enable this storage backend we use django-storages package...
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+# ...that uses AWS' boto3 library.
+# https://boto3.amazonaws.com/v1/documentation/api/latest/index.html
+#
+# Three required environment variables are:
+#  * AWS_STORAGE_BUCKET_NAME
+#  * AWS_ACCESS_KEY_ID
+#  * AWS_SECRET_ACCESS_KEY
+# The last two are picked up by boto3:
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html#environment-variables
 if "AWS_STORAGE_BUCKET_NAME" in os.environ:
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_AUTO_CREATE_BUCKET = True
+    # Add django-storages to the installed apps
+    INSTALLED_APPS = INSTALLED_APPS + ["storages"]
 
-    INSTALLED_APPS.append("storages")
-    if "AWS_S3_CUSTOM_DOMAIN" in os.environ:
-        AWS_S3_CUSTOM_DOMAIN = os.environ["AWS_S3_CUSTOM_DOMAIN"]
+    # https://docs.djangoproject.com/en/stable/ref/settings/#default-file-storage
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+
+    # Disables signing of the S3 objects' URLs. When set to True it
+    # will append authorization querystring to each URL.
+    AWS_QUERYSTRING_AUTH = False
+
+    # Do not allow overriding files on S3 as per Wagtail docs recommendation:
+    # https://docs.wagtail.io/en/stable/advanced_topics/deploying.html#cloud-storage
+    # Not having this setting may have consequences in losing files.
+    AWS_S3_FILE_OVERWRITE = False
+
+    # Default ACL for new files should be "private" - not accessible to the
+    # public. Images should be made available to public via the bucket policy,
+    # where the documents should use wagtail-storages.
+    AWS_DEFAULT_ACL = "private"
+
+    # We generally use this setting in the production to put the S3 bucket
+    # behind a CDN using a custom domain, e.g. media.llamasavers.com.
+    # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#cloudfront
+    if "AWS_S3_CUSTOM_DOMAIN" in os.environ:
+        AWS_S3_CUSTOM_DOMAIN = os.environ["AWS_S3_CUSTOM_DOMAIN"]
+
+    # When signing URLs is facilitated, the region must be set, because the
+    # global S3 endpoint does not seem to support that. Set this only if
+    # necessary.
+    if "AWS_S3_REGION_NAME" in os.environ:
+        AWS_S3_REGION_NAME = os.environ["AWS_S3_REGION_NAME"]
+
+    # This settings lets you force using http or https protocol when generating
+    # the URLs to the files. Set https as default.
+    # https://github.com/jschneier/django-storages/blob/10d1929de5e0318dbd63d715db4bebc9a42257b5/storages/backends/s3boto3.py#L217
+    AWS_S3_URL_PROTOCOL = os.environ.get("AWS_S3_URL_PROTOCOL", "https:")
+
+
+# Basic support for Google Cloud Storage (AWS S3 is recommended for bakerydemo)
 if "GS_BUCKET_NAME" in os.environ:
     GS_BUCKET_NAME = os.getenv("GS_BUCKET_NAME")
     GS_PROJECT_ID = os.getenv("GS_PROJECT_ID")
