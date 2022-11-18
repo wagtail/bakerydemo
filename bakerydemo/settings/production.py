808de9a93dc3d60df0d15b4a7d627bc6944ed9d8
Jake Howard <jake.howard@torchbox.com>
2022-11-18 12:44:41 +0000

    Support custom S3 domains

--- bakerydemo/settings/production.py
+++ bakerydemo/settings/production.py
@@ -105,11 +105,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 if "AWS_STORAGE_BUCKET_NAME" in os.environ:
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
     AWS_AUTO_CREATE_BUCKET = True
 
     INSTALLED_APPS.append("storages")
-    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    if "AWS_S3_CUSTOM_DOMAIN" in os.environ:
+        AWS_S3_CUSTOM_DOMAIN = os.environ["AWS_S3_CUSTOM_DOMAIN"]
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 if "GS_BUCKET_NAME" in os.environ:
