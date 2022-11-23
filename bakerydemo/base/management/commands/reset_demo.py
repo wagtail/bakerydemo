dc51dd3e77f183b1ed4234d5fca30e81a49ee27c
Jake Howard <jake.howard@torchbox.com>
2022-11-23 11:28:34 +0000

    Only reset demo on postgres
    Only postgres supports the `reset_schema` command. We error beforehand to avoid leaving the database in a broken state

--- bakerydemo/base/management/commands/reset_demo.py
+++ bakerydemo/base/management/commands/reset_demo.py
@@ -1,13 +1,20 @@
 from django.conf import settings
 from django.core.files.storage import default_storage
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from django.db import DEFAULT_DB_ALIAS
+from django_extensions.settings import POSTGRESQL_ENGINES
 from wagtail.documents import get_document_model
 from wagtail.images import get_image_model
 
 
 class Command(BaseCommand):
     def handle(self, **options):
+        if settings.DATABASES[DEFAULT_DB_ALIAS]["ENGINE"] not in POSTGRESQL_ENGINES:
+            raise CommandError(
+                "This command can be used only with PostgreSQL databases."
+            )
+
         # 1. (optional) Remove all objects from S3
         if "s3" in settings.DEFAULT_FILE_STORAGE:
             self.stdout.write("Removing files from S3")
