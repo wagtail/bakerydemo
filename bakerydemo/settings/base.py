66f260b39de9fb35982b0b9fb9b14ffbc9cd070f
Jake Howard <jake.howard@torchbox.com>
2023-04-26 13:24:06 +0100

    Fix import order

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import dj_database_url
 import os
+
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(PROJECT_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
