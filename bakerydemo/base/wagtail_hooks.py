b5b82083baf60689d1d6fb8557a22bf692f5f3d0
Sage Abdullah <sage.abdullah@torchbox.com>
2023-05-15 12:43:46 +0100

    Add CustomAccessibilityItem with all non-experimental rules enabled

--- bakerydemo/base/wagtail_hooks.py
+++ bakerydemo/base/wagtail_hooks.py
@@ -1,4 +1,5 @@
 from wagtail import hooks
+from wagtail.admin.userbar import AccessibilityItem
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
     ModelAdminGroup,
@@ -32,6 +33,18 @@ def register_icons(icons):
     return icons + [
         "wagtailfontawesomesvg/solid/suitcase.svg",
         "wagtailfontawesomesvg/solid/utensils.svg",
+    ]
+
+
+class CustomAccessibilityItem(AccessibilityItem):
+    axe_run_only = None
+
+
+@hooks.register("construct_wagtail_userbar")
+def replace_userbar_accessibility_item(request, items):
+    items[:] = [
+        CustomAccessibilityItem() if isinstance(item, AccessibilityItem) else item
+        for item in items
     ]
 
 
