eb955235e62ea2abb382e0a8b9129ac03bfe97fa
MikeMavrok <t8200104@aueb.gr>
2023-06-25 14:36:28 +0300

    reformat of apps.py

--- bakerydemo/base/apps.py
+++ bakerydemo/base/apps.py
@@ -4,7 +4,9 @@ from wagtail.images.apps import WagtailImagesAppConfig
 # to be added to all images in the project. In this case, the default attributes are "decoding": "async" and
 # "loading": "lazy", which can enhance the loading and decoding behavior of the images.
 
+
 class CustomImagesAppConfig(WagtailImagesAppConfig):
     default_attrs = {"decoding": "async", "loading": "lazy"}
+
 
 default_app_config = "bakerydemo.base.apps.CustomImagesAppConfig"
