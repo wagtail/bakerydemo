586f47016e049eb74693f05816ffe95b90f6e2bb
gzark1 <t8200043@aueb.gr>
2023-06-28 18:06:27 +0300

    Add import in blocks.py

--- bakerydemo/base/blocks.py
+++ bakerydemo/base/blocks.py
@@ -6,6 +6,7 @@ from wagtail.blocks import (
     StructBlock,
     TextBlock,
 )
+from wagtail.blocks.field_block import BooleanBlock
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
 
