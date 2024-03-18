99c2b0cdcb6c69c5b44e270c597e4980e1b83ae6
Aurel <aurel.pere@ikmail.com>
2024-03-18 01:00:03 +0100

    add tags in gallery_tags.py but not in html to let forker choose custom javascript, ImagePage or something else

--- bakerydemo/base/templatetags/gallery_tags.py
+++ bakerydemo/base/templatetags/gallery_tags.py
@@ -1,15 +1,21 @@
 from django import template
+from taggit.models import Tag
 from wagtail.images.models import Image
 
 register = template.Library()
 
 
 # Retrieves a single gallery item and returns a gallery of images
+# and their tags
 @register.inclusion_tag("tags/gallery.html", takes_context=True)
 def gallery(context, gallery):
     images = Image.objects.filter(collection=gallery)
-
+    ids = [k.id for k in images]
+    tags = list(
+        Tag.objects.filter(taggit_taggeditem_items__object_id__in=ids).distinct()
+    )
     return {
+        "tags": tags,
         "images": images,
         "request": context["request"],
     }
