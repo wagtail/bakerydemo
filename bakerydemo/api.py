4571afb5b8c11f45ab97cf8661e56a334416b698
Raghad Dahi <raghaddahi27@gmail.com>
2026-04-10 13:07:13 +0200

    feat: add Person API endpoint

--- bakerydemo/api.py
+++ bakerydemo/api.py
@@ -4,6 +4,8 @@ from wagtail.contrib.redirects.api import RedirectsAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
 
+from bakerydemo.base.api import PersonApiViewSet
+
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter("wagtailapi")
 
@@ -15,3 +17,4 @@ api_router.register_endpoint("pages", PagesAPIViewSet)
 api_router.register_endpoint("images", ImagesAPIViewSet)
 api_router.register_endpoint("documents", DocumentsAPIViewSet)
 api_router.register_endpoint("redirects", RedirectsAPIViewSet)
+api_router.register_endpoint("persons", PersonApiViewSet)
