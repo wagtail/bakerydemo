9582d2aebcdc47ffa1b441301b4c106896fdc8b4
Srishti-j18 <srishti33164jaiswal@gmail.com>
2026-06-21 01:01:29 +0530

    Add page translation metadata helpers

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
